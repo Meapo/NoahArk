ab330aa957c1954d3086c8bed5f5bb0d9adba4e0
Cubafehis0 <2656023557@qq.com>
2020-12-11 18:55:44 +0800

    船和云的动画

---
+++ Assets/BoatFloat.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatFloat : MonoBehaviour
+{
+    public float moveSpeed;
+    int[, ] dir = new int[,] { { 1, 1 }, { 1, -1 }, { -1, -1 }, { -1, 1 } };
+    int cnt = 0;
+    public int movCnt;
+    int index;
+    private void Start()
+    {
+        cnt = 0;
+        index = 0;
+    }
+    private void FixedUpdate()
+    {
+        Vector2 movDir;
+        movDir.x = Random.value*dir[index,0];
+        movDir.y = Random.value*dir[index,1];
+        transform.Translate(movDir * moveSpeed * Time.deltaTime);
+        cnt++;
+        if(cnt>=movCnt)
+        {
+            cnt = 0;
+            index = (index + 1) % 4;
+        }
+    }
+}
---
+++ Assets/CloudMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMove : MonoBehaviour
+{
+    public float endPosX;
+    public float movSpeed;
+    RectTransform[] rects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rects = GetComponentsInChildren<RectTransform>();
+        Debug.Log(rects.Length);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        for(int i=0;i<rects.Length-1;i++)
+        {
+            rects[i + 1].Translate(new Vector3( movSpeed * Time.deltaTime, 0,0));
+            if(rects[i+1].anchoredPosition.x>endPosX)
+            {
+                //Debug.Log(tran.sizeDelta.x);
+                rects[i+1].Translate(new Vector3(-rects[i+1].sizeDelta.x * 2, 0));
+            }
+        }
+    }
+}
